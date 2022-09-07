4badc8163e6f99bfd07f64ea8c59be4361a49b4e
yunho <kimyouknow@naver.com>
2022-09-07 17:10:28 +0900

    Design: GNB에 theme로 typography적용 테스트

--- src/components/GlobalNavigation/GlobalNavigation.style.ts
+++ src/components/GlobalNavigation/GlobalNavigation.style.ts
@@ -5,20 +5,20 @@ export const Container = styled.header<{ isHidden: boolean }>`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  box-sizing: border-box;
   position: fixed;
   top: ${({ isHidden }) => (isHidden ? -60 : 0)}px;
   left: 0;
   right: 0;
-  padding: 16px;
+  padding: 1.5rem 2rem 1rem 2rem;
   background-color: #ffffff;
-  box-shadow: 0 0 8px rgba(255, 255, 255, 0.85);
+  box-shadow: rgba(0, 0, 0, 0.08) 0px 0px 15px;
   backdrop-filter: blur(5px);
   opacity: ${({ isHidden }) => (isHidden ? 0 : 1)};
   transition: top 0.5s, opacity 0.5s;
   z-index: 999;
   width: 100vw;
-  height: 100px;
-  box-sizing: border-box;
+  ${({ theme: { typography } }) => typography.linkMedium};
 `
 
 export const BlogTitle = styled.div``
