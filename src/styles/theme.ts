c1a9fc603e427b60592bb1e09d1caafd39af48bd
yunho <kimyouknow@naver.com>
2022-09-07 17:49:57 +0900

    Design: GREYSCALE 항목 추가
    60, 100 ~ 800

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -24,7 +24,15 @@ const FONTS = {
 
 const COLORS = {
   GREYSCALE: {
-    '000': '#F5F5F5',
+    '050': '#FAFAFA',
+    '100': '#F5F5F5',
+    '200': '#EEEEEE',
+    '300': '#E0E0E0',
+    '400': '#BDBDBD',
+    '500': '#9E9E9E',
+    '600': '#757575',
+    '700': '#616161',
+    '800': '#424242',
     '900': '#212121',
   },
   BLACK: '#000',
@@ -89,7 +97,7 @@ const typography = {
   `,
 }
 
-const theme = { color, FONTS, typography }
+const theme = { FONTS, typography, COLORS, color }
 
 export default theme
 
