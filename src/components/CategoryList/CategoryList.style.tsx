4aac7966bb42608e0d59af7467bdccb27d53b04c
yunho <kimyouknow@naver.com>
2022-09-08 11:54:53 +0900

    Fix: CategoryList에서 AllCategoryTag 분리

--- src/components/CategoryList/CategoryList.style.tsx
+++ src/components/CategoryList/CategoryList.style.tsx
@@ -12,9 +12,7 @@ interface GatsbyLinkProps extends CategoryItemProps {
   to: string
 }
 
-export const Container = styled.div``
-
-export const CategoryListWrapper = styled.div`
+export const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
   width: 768px;
@@ -23,12 +21,16 @@ export const CategoryListWrapper = styled.div`
 
 export const CategoryItem = styled(({ active, ...props }: GatsbyLinkProps) => <Link {...props} />)`
   cursor: pointer;
-  margin-right: 20px;
+  margin-right: 12px;
   padding: 5px 0;
   color: ${({ active, theme: { COLORS } }) => (active ? COLORS.BLACK : COLORS.GREYSCALE['700'])};
-  ${({ theme: { typography } }) => typography.linkMedium};
-
+  ${({ theme: { typography } }) => typography.linkBase};
+  vertical-align: center;
   &:last-of-type {
     margin-right: 0;
   }
+`
+
+export const AllCategoryTag = styled(CategoryItem)`
+  ${({ theme: { typography } }) => typography.linkMedium};
 `
--- src/components/CategoryList/index.tsx
+++ src/components/CategoryList/index.tsx
@@ -8,14 +8,18 @@ export interface CategoryListProps {
 }
 
 const CategoryList = ({ selectedCategory, categoryList }: CategoryListProps) => {
+  const { All: AllCategoryTag, ...restCategoryList } = categoryList
   return (
-    <S.CategoryListWrapper>
-      {Object.entries(categoryList).map(([name, count]) => (
+    <S.Container>
+      <S.AllCategoryTag to="/?category=All" active>
+        전체 {AllCategoryTag}개의 포스팅
+      </S.AllCategoryTag>
+      {Object.entries(restCategoryList).map(([name, count]) => (
         <S.CategoryItem key={name} to={`/?category=${name}`} active={name === selectedCategory}>
           #{name}({count})
         </S.CategoryItem>
       ))}
-    </S.CategoryListWrapper>
+    </S.Container>
   )
 }
 
