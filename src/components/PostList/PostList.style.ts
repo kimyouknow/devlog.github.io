9d9a0a68dd4b2429301d9f0acc618be6a99c19a7
yunho <kimyouknow@naver.com>
2022-09-19 16:41:15 +0900

    Design: 포스트리스트 grid에서 한 줄 정렬로 변경

--- src/components/PostList/PostList.style.ts
+++ src/components/PostList/PostList.style.ts
@@ -3,15 +3,14 @@ import { Link } from 'gatsby'
 
 // PostList에 대한 스타일
 export const Container = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 20px;
   width: 768px;
   margin: 0 auto;
   padding: 50px 0 100px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 
   @media (max-width: 768px) {
-    grid-template-columns: 1fr;
     width: 100%;
     padding: 50px 20px;
   }
