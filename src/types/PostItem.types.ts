1d0b95eeaf982babaf633db735ee4603cfff08f1
yunho <kimyouknow@naver.com>
2023-01-12 23:00:54 +0900

    Feat: postItem 타입에 post 세부 타입 추가
    PostFrontmatterType: thumbnail추가
    PostListItemType: tableOfContents추가
    PostType
    PostPageItemType

--- src/types/PostItem.types.ts
+++ src/types/PostItem.types.ts
@@ -1,16 +1,39 @@
-export type PostFrontmatterType = {
+import { GatsbyImageDataType } from './gatsby.type'
+
+export interface PostFrontmatterType {
   title: string
   date: string
   categories: string[]
   summary: string
+  thumbnail: {
+    childImageSharp: {
+      gatsbyImageData: GatsbyImageDataType
+    }
+  }
 }
 
-export type PostListItemType = {
+export interface PostListItemType {
   node: {
+    tableOfContents: string
     id: string
     fields: {
       slug: string
     }
+    frontmatter: PostFrontmatterType
+  }
+}
+
+export interface PostType {
+  node: {
+    id: string
+    frontmatter: PostFrontmatterType
+  }
+}
+
+export interface PostPageItemType {
+  node: {
+    tableOfContents: string
+    html: string
     frontmatter: PostFrontmatterType
   }
 }
