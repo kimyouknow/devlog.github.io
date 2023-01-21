9b8edf473f8042608f5f6fff10678ba760c6da1b
yunho <kimyouknow@naver.com>
2023-01-21 22:07:56 +0900

    Feat: blog config 관련 변수 추가

---
+++ src/constant/blog.config.ts
@@ -0,0 +1,20 @@
+const BLOG_CONFIG = {
+  title: `Yunho.devlog`,
+  author: `Yunho`,
+  introduction: `🙌 안녕하세요.김윤호(도리)의 블로그입니다. 저는 아래와 같은 고민을 하며 개발을 하고 있습니다.`,
+  siteUrl: 'https://kimyouknow.github.io',
+  social: {
+    email: 'kimyouknow@naver.com',
+    github: `https://github.com/kimyouknow`,
+  },
+  info: {
+    til: '',
+    about: '',
+  },
+  icon: `content/assets/pencil.png`,
+  comment: {
+    utterances: 'kimyouknow/kimyouknow.github.io',
+  },
+}
+
+export default BLOG_CONFIG
