cf85583dfa28031511a131ab8c195c31ad992154
yunho <kimyouknow@naver.com>
2023-01-21 22:08:38 +0900

    Feat: About페이지

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,23 @@
+import styled from '@emotion/styled'
+
 import Layout from '@/Layout'
 
 const About = () => {
-  return <Layout>About</Layout>
+  return (
+    <Layout>
+      <LayoutStyle>
+        <h1>준비중입니다..😅</h1>
+      </LayoutStyle>
+    </Layout>
+  )
 }
+
+const LayoutStyle = styled.div`
+  width: 768px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 export default About
