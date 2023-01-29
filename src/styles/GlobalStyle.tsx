13c71c11e2207566642ee7db969e8b83f72be73d
yunho <kimyouknow@naver.com>
2023-01-29 16:34:12 +0900

    Design: css variable 추가(다크, 라이트 모드)

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -7,6 +7,50 @@ import Normalize from './Normalize'
 const styles = css`
   ${fonts}
   ${Normalize}
+  :root {
+    --color-background: #f3f5f7;
+    --color-background-secondary: #ffffff;
+    --color-text: #00214d;
+    --color-heading-text: #000;
+    --color-paragraph: #1b2e46;
+    --color-anchor-text: #0076d1;
+    --color-code-background: #d7dad3;
+    --color-code-text: #212529;
+    --color-table-background-color: #fff;
+    --color-table-background-color-second: #f6f8fa;
+    --color-table-border: #d0d7de;
+    --color-navigation-shadow: rgba(0, 0, 0, 0.08);
+
+    /* --color-primary: 
+    --color-secondary:
+    --color-tertiary: */
+    --color-white: #f3f5f7;
+    --color-black: #222;
+    --color-gray: #5a6072;
+    --color-gray-1000: #21232c;
+    --color-overlay: #61646bcc;
+  }
+
+  @media (prefers-color-scheme: dark) {
+    :root {
+      --color-background: #242629;
+      --color-background-secondary: #151519;
+      --color-text: #dbdbdb;
+      --color-heading-text: #fff;
+      --color-paragraph: #abb5c3;
+      --color-anchor-text: #0076d1;
+      --color-code-background: #495057;
+      --color-code-text: #f8f9fa;
+      --color-table-background-color: #202b38;
+      --color-table-background-color-second: #0d1117;
+      --color-table-border: #424242;
+      --color-navigation-shadow: rgba(0, 0, 0, 0.08);
+
+      /* --color-primary:  */
+      /* --color-secondary:  */
+      /* --color-tertiary:  */
+    }
+  }
   * {
     font-family: 'AppleSDGothicNeo', 'Noto Sans', 'sans-serif';
     box-sizing: border-box;
