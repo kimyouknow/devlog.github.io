dc700a19ae021af707feccdc342d158d222de431
yunho <kimyouknow@naver.com>
2023-01-29 20:12:27 +0900

    Refactor: useUtterance 분리

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -1,31 +1,9 @@
-import { useEffect, useRef } from 'react'
-
-const customUtteranceAttribute = {
-  src: 'https://utteranc.es/client.js',
-  repo: 'kimyouknow/kimyouknow.github.io',
-  'issue-term': 'pathname',
-  theme: 'github-light',
-  label: '💬 comments',
-  crossorigin: 'anonymous',
-  async: 'true',
-}
+import useUtterance from '@/hooks/useUtterance'
 
 const Comment = () => {
-  const element = useRef<HTMLDivElement>(null)
+  const { utteranceElement } = useUtterance()
 
-  useEffect(() => {
-    if (element.current === null) return
-    const utterances: HTMLScriptElement = document.createElement('script')
-    const attributes = customUtteranceAttribute
-
-    Object.entries(attributes).forEach(([key, value]) => {
-      utterances.setAttribute(key, value)
-    })
-
-    element.current.appendChild(utterances)
-  }, [])
-
-  return <div ref={element} />
+  return <div ref={utteranceElement} />
 }
 
 export default Comment
---
+++ src/hooks/useUtterance.ts
@@ -0,0 +1,47 @@
+import { useEffect, useRef } from 'react'
+
+import { useThemeModeProviderState } from '@/context/ThemeMode.Provider'
+
+const customUtteranceAttribute = {
+  src: 'https://utteranc.es/client.js',
+  repo: 'kimyouknow/kimyouknow.github.io',
+  'issue-term': 'pathname',
+  theme: 'github-light',
+  label: '💬 comments',
+  crossorigin: 'anonymous',
+  async: 'true',
+}
+const useUtterance = () => {
+  const { isDarkMode } = useThemeModeProviderState()
+  const utteranceTheme = isDarkMode ? 'github-dark' : 'github-light'
+  const utteranceElement = useRef<HTMLDivElement>(null)
+
+  const createUtterance = () => {
+    const utterances: HTMLScriptElement = document.createElement('script')
+    const attributes = { ...customUtteranceAttribute, theme: utteranceTheme }
+
+    Object.entries(attributes).forEach(([key, value]) => {
+      utterances.setAttribute(key, value)
+    })
+
+    utteranceElement.current?.appendChild(utterances)
+  }
+
+  const setThemeUtterance = (iframe: HTMLIFrameElement) => {
+    const msg = {
+      type: 'set-theme',
+      theme: utteranceTheme,
+    }
+    iframe.contentWindow?.postMessage(msg, 'https://utteranc.es')
+  }
+
+  useEffect(() => {
+    if (utteranceElement.current === null) return
+    const utteranceIframe = document.querySelector('iframe')
+    utteranceIframe ? setThemeUtterance(utteranceIframe) : createUtterance()
+  }, [isDarkMode])
+
+  return { utteranceElement }
+}
+
+export default useUtterance
