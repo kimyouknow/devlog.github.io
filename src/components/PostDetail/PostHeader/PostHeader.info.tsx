68fcd5878b035f746ac7edf6fe4f87afb51e87a5
yunho <kimyouknow@naver.com>
2023-01-29 16:38:36 +0900

    Fix: PostHeader에 css variable 적용

--- src/components/PostDetail/PostHeader/PostHeader.info.tsx
+++ src/components/PostDetail/PostHeader/PostHeader.info.tsx
@@ -1,6 +1,7 @@
-import styled from '@emotion/styled'
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import * as S from './PostHeader.style'
 
 export interface PostHeadInfoProps {
   title: string
@@ -12,84 +13,17 @@ const PostHeaderInfo = ({ title, date, categories }: PostHeadInfoProps) => {
   const goBackPage = () => window.history.back()
 
   return (
-    <PostHeadInfoWrapper>
-      <PrevPageIcon onClick={goBackPage}>
+    <S.PostHeadInfoWrapper>
+      <S.PrevPageIcon onClick={goBackPage}>
         <FontAwesomeIcon icon={faArrowLeft} />
-      </PrevPageIcon>
-      <Title>{title}</Title>
-      <PostData>
+      </S.PrevPageIcon>
+      <S.Title>{title}</S.Title>
+      <S.PostData>
         <div>{categories.map(v => v.toUpperCase()).join(' / ')}</div>
         <div>{date}</div>
-      </PostData>
-    </PostHeadInfoWrapper>
+      </S.PostData>
+    </S.PostHeadInfoWrapper>
   )
 }
 
 export default PostHeaderInfo
-
-const PostHeadInfoWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 768px;
-  height: 100%;
-  margin: 0 auto;
-  padding: 60px 0;
-  color: #ffffff;
-
-  @media (max-width: 768px) {
-    width: 100%;
-    padding: 40px 20px;
-  }
-`
-
-const PrevPageIcon = styled.div`
-  display: grid;
-  place-items: center;
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-  background: #ffffff;
-  color: #000000;
-  font-size: 22px;
-  cursor: pointer;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
-
-  @media (max-width: 768px) {
-    width: 30px;
-    height: 30px;
-    font-size: 18px;
-  }
-`
-
-const Title = styled.div`
-  display: -webkit-box;
-  overflow: hidden;
-  overflow-wrap: break-word;
-  margin-top: auto;
-  text-overflow: ellipsis;
-  white-space: normal;
-  -webkit-line-clamp: 2;
-  -webkit-box-orient: vertical;
-  font-size: 45px;
-  font-weight: 800;
-
-  @media (max-width: 768px) {
-    font-size: 30px;
-  }
-`
-
-const PostData = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-top: 10px;
-  font-size: 18px;
-  font-weight: 700;
-
-  @media (max-width: 768px) {
-    flex-direction: column;
-    align-items: flex-start;
-    font-size: 15px;
-    font-weight: 400;
-  }
-`
