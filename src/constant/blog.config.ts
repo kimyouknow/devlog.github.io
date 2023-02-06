c543611d0bd44245817c5b0e48b2a38ac0c06af4
yunho <kimyouknow@naver.com>
2023-02-06 22:55:27 +0900

    Fix: blog config 타입 수정

--- src/constant/blog.config.ts
+++ src/constant/blog.config.ts
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { ConfigType } from '@/types/gatsby.type'
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment
-const blogConfig: ConfigType = require('../../blog-config')
+// eslint-disable-next-line @typescript-eslint/no-var-requires,
+const blogConfig = require('../../blog-config')
 
-const BLOG_CONFIG = {
+const BLOG_CONFIG: ConfigType = {
   ...blogConfig,
 }
 
