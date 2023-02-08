c4a9b7467e930c3b7a42638a621243cdaa678d89
yunho <kimyouknow@naver.com>
2023-02-08 16:47:35 +0900

    Design: media의 breakpoints에 xLarge속성 추가
    xLarge: 1360

--- src/styles/media.ts
+++ src/styles/media.ts
@@ -1,13 +1,15 @@
 // 네이밍 참고 desktop, mobile이 아닌 large, small로 작성한 이유 : https://github.com/airbnb/javascript/tree/master/css-in-javascript#naming
-type DeviceType = 'large' | 'medium' | 'small'
+type DeviceType = 'xLarge' | 'large' | 'medium' | 'small'
 
 const breakpoints: Record<DeviceType, number> = {
+  xLarge: 1360,
   large: 1200,
   medium: 768,
   small: 600,
 }
 
 const media = {
+  xLarge: `screen and (max-width: ${breakpoints.xLarge}px)`,
   large: `screen and (max-width: ${breakpoints.large}px)`,
   medium: `screen and (max-width: ${breakpoints.medium}px)`,
   small: `screen and (max-width: ${breakpoints.small}px)`,
