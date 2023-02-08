53955a9912f544c836548eac0ca36829a3420536
yunho <kimyouknow@naver.com>
2023-02-08 16:49:50 +0900

    Design: Layout페이지 css variable적용

--- src/Layout/Layout.style.ts
+++ src/Layout/Layout.style.ts
@@ -3,7 +3,8 @@ import styled from '@emotion/styled'
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  padding-top: 100px;
+  width: 100%;
+  padding-top: var(--padding-xl);
   color: var(--color-text);
   background-color: var(--color-background);
 `
